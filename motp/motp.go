a045dff4b8e16a7ad5db90b735dfa2b11417a1b9
marthjod@gmail.com <marthjod@gmail.com>
2017-09-20 17:33:07 +0200

    motp: secret may be passed from clipboard

--- motp/motp.go
+++ motp/motp.go
@@ -10,37 +10,41 @@ import (
 	"strconv"
 	"time"
 
-	speakeasy "github.com/bgentry/speakeasy"
+	"github.com/atotto/clipboard"
+	"github.com/bgentry/speakeasy"
 )
 
 func main() {
+
+	currentUser, _ := user.Current()
+
 	var (
-		currentUser *user.User
-		outFile     string
-		output      string
-		vpnUser     string
-		hash        string
-		otp         string
-		secret      string
-		pin         string
-		err         error
-		nowStr      string
+		output          string
+		hash            string
+		otp             string
+		secret          string
+		pin             string
+		err             error
+		nowStr          string
+		vpnUser         = flag.String("u", currentUser.Name, "OpenVPN user")
+		outFile         = flag.String("o", "", "Output file path")
+		secretClipboard = flag.Bool("s", false, "Read secret from clipboard")
 	)
-
-	currentUser, _ = user.Current()
-
-	flag.StringVar(&vpnUser, "u", currentUser.Name, "OpenVPN user")
-	flag.StringVar(&outFile, "o", "", "Output file path")
 	flag.Parse()
 
-	if outFile == "" {
+	if *outFile == "" {
 		fmt.Println("Must provide output file path to write OTP to.")
 		os.Exit(1)
 	}
 
-	if secret, err = speakeasy.Ask("Secret (not echoed): "); err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+	if *secretClipboard {
+		secret, _ = clipboard.ReadAll()
+	}
+	if secret == "" {
+		if secret, err = speakeasy.Ask("Secret (not echoed): "); err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
 	}
 	if pin, err = speakeasy.Ask("PIN (not echoed): "); err != nil {
 		fmt.Println(err.Error())
@@ -50,6 +54,6 @@ func main() {
 	nowStr = strconv.FormatInt(time.Now().Unix()/10, 10)
 	hash = fmt.Sprintf("%x", md5.Sum([]byte(nowStr+secret+pin)))
 	otp = hash[:6]
-	output = fmt.Sprintf("%s\n%s", vpnUser, otp)
-	ioutil.WriteFile(outFile, []byte(output), 0644)
+	output = fmt.Sprintf("%s\n%s", *vpnUser, otp)
+	ioutil.WriteFile(*outFile, []byte(output), 0644)
 }
